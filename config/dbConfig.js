52d3b9cf77c050bac1256893537e9e6cff3cd758
Gulshan Dubey <gulshan.dubey@krishnacollege.ac.in>
2022-09-02 19:07:06 +0530

    twitter_clone(nagarro)

---
+++ config/dbConfig.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+require('dotenv').config()
+const dotenv=require('dotenv');
+const harperive = require('harperive');
+
+const DB_CONFIG = {
+  harperHost: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  schema: process.env.DB_SCHEMA
+}
+
+const Client = harperive.Client;
+const db = new Client(DB_CONFIG);
+
+module.exports = db;
